06e3ef3f184929c67cb8f66011f2d56e879bfa26
Steelyhands <93715151+Steelyhands@users.noreply.github.com>
2024-08-11 18:42:39 +0100

    added store functionality

---
+++ Controllers/storeController.js
@@ -0,0 +1,55 @@
+const Store = require('../Models/storeModel');
+const storeDAO = new store();
+
+exports.show_login = function (req, res) {
+    res.render("store/login");
+};
+
+exports.handle_login = function (req, res) {
+    res.render("home", {
+      user: "user"
+    });
+};
+
+exports.update_store = function (req, res) {
+    const address = req.body.address;
+    const postcode = req.body.postcode;
+    const telephone = req.body.telephone;
+    const password = req.body.password;
+  
+    if (!address || !postcode || !telephone || !password) {
+      res.send(401, "missing values");
+      return;
+    }
+  
+    pantryDao.updatePantry(address, postcode, telephone, password);
+    res.redirect("/store/storeAccount");
+};
+
+exports.delete_store = function(req, res) {
+    const userId = req.body.userId;
+  
+    // Call the deleteUser method from your userModel to delete the user
+    storeDao.deleteUser(userId, (err) => {
+        if (err) {
+            // Handle error
+            console.error("Error deleting user:", err);
+            res.status(500).send("Internal server error");
+            return;
+        }
+        // Redirect back to the user database page after deletion
+        res.redirect("/store/stores");
+    });
+};
+
+exports.show_stores = function (req, res) {
+    pantryDao.loadAllPantries()
+      .then((list) => {
+        res.render("store/stores", {
+          users: list,
+        });
+      })
+      .catch((err) => {
+        console.log("promise rejected", err);
+      });
+};
