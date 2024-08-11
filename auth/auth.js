e9353b9591a5e3fc37148ebafd600f39b607c521
Steelyhands <93715151+Steelyhands@users.noreply.github.com>
2024-08-11 23:56:18 +0100

    played around with auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -1,19 +1,20 @@
+// Import required modules
 const bcrypt = require("bcrypt");
 const userModel = require("../Models/userModel");
-const jwt = require("jsonwebtoken");
 const user = new userModel;
-const adminModel = require('../models/adminModel');
+const adminModel = require("../Models/adminModel");
 const admin = new adminModel;
+const jwt = require("jsonwebtoken");
 const barnardosModel = require('../models/barnardosModel');
 const newLocation = new barnardosModel;
 
-
-// The 'login' function
+// User login function
 exports.login = function (req, res, next) {
     let username = req.body.username;
     let password = req.body.password;
 
-    userModel.lookup(username, function (err, user) {
+    // Lookup user
+    user(username, function (err, user) {
         if (err) { 
             console.log("error looking up user", err); 
             return res.status(401).send();
@@ -22,13 +23,14 @@ exports.login = function (req, res, next) {
             console.log("user ", username, " not found"); 
             return res.render("/register");
         }
-        //compare provided password with stored password
+
+        // Compare provided password with stored password
         bcrypt.compare(password, user.password, function (err, result) {
             if (result) {
                 let payload = { username: username, isAdmin: isAdmin.role, id: user._id}; 
                 let accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET);
                 res.cookie("jwt", accessToken);
-                //and then pass onto the next middleware
+                // Pass onto the next middleware
                 next();
             } else { 
                 return res.status(403).send();
@@ -37,7 +39,38 @@ exports.login = function (req, res, next) {
     });
 }
 
-// The 'verify' function
+// Admin login function
+exports.admin_login = function (req, res, next) {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    // Lookup admin
+    admin.lookup(username, function (err, user) {
+        if (err) { 
+            console.log("error looking up user", err); 
+            return res.status(401).send();
+        } 
+        if (!admin) { 
+            console.log("Admin ", username, " not found"); 
+            return res.render("/login");
+        }
+
+        // Compare provided password with stored password
+        bcrypt.compare(password, user.password, function (err, result) {
+            if (result) {
+                let payload = { username: username, isAdmin: isAdmin.role, id: user._id}; 
+                let accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET);
+                res.cookie("jwt", accessToken);
+                // Pass onto the next middleware
+                next();
+            } else { 
+                return res.status(403).send();
+            }
+        });
+    });
+}
+
+// Verify function
 exports.verify = function (req, res, next) {
     let accessToken = req.cookies.jwt; 
     if (!accessToken) { 
@@ -48,11 +81,28 @@ exports.verify = function (req, res, next) {
         payload = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
         next(); 
     } catch (e) {
-        //if an error occurred return request unauthorized error
+        // If an error occurred return request unauthorized error
         res.status(401).send();
     }
 }
 
+// Verify user function
+exports.verifyUser = function (req, res, next) 
+{
+  let accessToken = req.cookies.jwt;
+    let payload = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+     if (payload.role != "admin" && payload.role != "user") {
+    return res.status(403).send();
+  }
+     try {
+    next();
+    } catch (e) {
+    // Unauthorized error
+    res.status(401).send();
+  }
+};
+
+// Verify admin function
 exports.verifyAdmin = function (req, res, next) {
   let accessToken = req.cookies.jwt;
     let payload = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
@@ -63,10 +113,28 @@ exports.verifyAdmin = function (req, res, next) {
             next();
         } 
     catch (e) {
-        //unauthorised acxcess error
+        // Unauthorized access error
         res.status(401).send();
         }
 };
+
+// Verify store function
+exports.verifyStore = function (req, res, next) 
+{
+  let accessToken = req.cookies.jwt;
+    let payload = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+  if (payload.role != "admin" && payload.role != "pantry") {
+    return res.status(403).send();
+  }
+  try {
+    next();
+  } 
+  catch (e) {
+    // Unauthorized error
+    res.status(401).send();
+  }
+};
+
 
 // Export the functions (no longer needed as using exports.)
 //module.exports = {
