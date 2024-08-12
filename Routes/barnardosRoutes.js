364657a16caeb9fc999684ff7a079ab8606fc0db
Steelyhands <93715151+Steelyhands@users.noreply.github.com>
2024-08-12 04:41:48 +0100

    added products to navbar

--- Routes/barnardosRoutes.js
+++ Routes/barnardosRoutes.js
@@ -31,6 +31,48 @@ router.get("/signup", (req, res) => {
     res.render('signup');
 });
 
+// handling 404 errors
+//router.use(function (req, res) {
+    //res.status(404);
+    //res.type('text/plain');
+   // res.send('404 Not found.');
+//});
+
+// handling internal server errors
+router.use(function (err, req, res, next) {
+    res.status(500);
+    res.type('text/plain');
+    res.send('Internal Server Error.');
+});
+
+router.post('/login', login,controller.handle_login);
+
+// Define routes for different paths
+// landing page
+router.get(["/","Home","/Homepage"], controller.home_page);
+
+// about page
+router.get("/about", controller.about_page);
+// contact page
+router.get("/contact", controller.contact_page);
+// home page
+router.get("/home", controller.home_page);
+
+// login page
+router.get("/login", controller.login_page);
+
+// signup page
+router.get("/signup", (req, res) => {
+    res.render('signup');
+});
+
+// handling 404 errors
+//router.use(function (req, res) {
+    //res.status(404);
+    //res.type('text/plain');
+   // res.send('404 Not found.');
+//});
+
 // handling internal server errors
 router.use(function (err, req, res, next) {
     res.status(500);
@@ -42,16 +84,18 @@ router.use(function (err, req, res, next) {
 // Route for handling form submissions to '/signup'
 router.post('/signup', (req, res) => {
     // Access form data from req.body
-    const { id } = req.body;
-    const user = new User(id);
-    user.createUser(id, (err, newUser) => {
-        if (err) {
-            res.status(500).send({ error: err.message });
-        } else {
-            res.status(200).send({ message: 'User created successfully.' });
-        }
+    const { userId } = req.body;
+
+    // Here you would typically validate the input data and save the new user to your database
+    // For example:
+    const user = new User(userId);
+    user.createUser()
+        .then(() => res.status(200).send({ message: 'User created successfully.' }))
+        .catch(err => res.status(500).send({ error: err.message }));
     });
-});
+
+// Export the router for use in other files
+module.exports = router;
 
 
 // Export the router for use in other files
