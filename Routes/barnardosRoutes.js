a397180cc9ae86aca8dd12ccc438f32642ec7c07
Steelyhands <93715151+Steelyhands@users.noreply.github.com>
2024-08-12 05:02:29 +0100

    trying to fix issues with deployment

--- Routes/barnardosRoutes.js
+++ Routes/barnardosRoutes.js
@@ -6,11 +6,7 @@ const router = express.Router();
 const controller = require('../controllers/barnardosController.js');
 const postController = require('../controllers/postController.js');
 const User = require('../Models/userModel');
-const {login, verify} = require('../auth/auth') // Add verify here
-
-
-
-router.post('/login', login,controller.handle_login);
+const {login, verify} = require('../auth/auth'); // Add verify here
 
 // Define routes for different paths
 // landing page
@@ -30,56 +26,6 @@ router.get("/login", controller.login_page);
 router.get("/signup", (req, res) => {
     res.render('signup');
 });
-
-// handling 404 errors
-//router.use(function (req, res) {
-    //res.status(404);
-    //res.type('text/plain');
-   // res.send('404 Not found.');
-//});
-
-// handling internal server errors
-router.use(function (err, req, res, next) {
-    res.status(500);
-    res.type('text/plain');
-    res.send('Internal Server Error.');
-});
-
-router.post('/login', login,controller.handle_login);
-
-// Define routes for different paths
-// landing page
-router.get(["/","Home","/Homepage"], controller.home_page);
-
-// about page
-router.get("/about", controller.about_page);
-// contact page
-router.get("/contact", controller.contact_page);
-// home page
-router.get("/home", controller.home_page);
-
-// login page
-router.get("/login", controller.login_page);
-
-// signup page
-router.get("/signup", (req, res) => {
-    res.render('signup');
-});
-
-// handling 404 errors
-//router.use(function (req, res) {
-    //res.status(404);
-    //res.type('text/plain');
-   // res.send('404 Not found.');
-//});
-
-// handling internal server errors
-router.use(function (err, req, res, next) {
-    res.status(500);
-    res.type('text/plain');
-    res.send('Internal Server Error.');
-});
-
 
 // Route for handling form submissions to '/signup'
 router.post('/signup', (req, res) => {
@@ -92,11 +38,21 @@ router.post('/signup', (req, res) => {
     user.createUser()
         .then(() => res.status(200).send({ message: 'User created successfully.' }))
         .catch(err => res.status(500).send({ error: err.message }));
-    });
+});
 
-// Export the router for use in other files
-module.exports = router;
+// handling 404 errors
+//router.use(function (req, res) {
+    //res.status(404);
+    //res.type('text/plain');
+   // res.send('404 Not found.');
+//});
 
+// handling internal server errors
+router.use(function (err, req, res, next) {
+    res.status(500);
+    res.type('text/plain');
+    res.send('Internal Server Error.');
+});
 
 // Export the router for use in other files
 module.exports = router;
